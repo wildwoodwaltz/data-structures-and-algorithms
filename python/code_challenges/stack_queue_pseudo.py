14f74630dd5a7b1fed3a3c4e719fca4feb80d027
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-02 13:08:51 -0700

    finished enqueue method

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,15 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    
+    def __init__(self):
+        self.inbox = Stack()
+        self.outbox = Stack()
+
+    def enqueue(self,value):
+        '''
+        enqueus items onto inbox stack
+        '''
+        self.inbox.push(value)
+
+
