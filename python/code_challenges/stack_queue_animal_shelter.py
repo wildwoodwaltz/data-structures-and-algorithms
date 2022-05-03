377aa562ad90f1f3e1ea32f331dfef231ae844c2
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-03 09:47:32 -0700

    added enqueue and dequeue methods to animal shelter

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,7 +2,23 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.cats = Queue()
+        self.dogs = Queue()
+
+    def enqueue(self, animal):
+        if isinstance(animal, Cat):
+            self.cats.enqueue(animal)
+        else:
+            self.dogs.enqueue(animal)
+
+    def dequeue(self, pref):
+        if pref == "cat" and self.cats:
+            return self.cats.dequeue()
+        if pref == "dog" and self.dogs:
+            return self.dogs.dequeue()
+        else:
+            return None
 
 
 class Dog:
