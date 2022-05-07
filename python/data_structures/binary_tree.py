de0ac851f615187337fdff822bbf9db1c931f7ca
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-06 20:35:07 -0700

    Added binary tree and iterative functions

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,55 @@
+
 class BinaryTree:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        def walk(root, data):
+            if not root:
+                return
+
+            data.append(root.value)
+            walk(root.left, data)
+            walk(root.right, data)
+
+        data_values = []
+        walk(self.root, data_values)
+        return data_values
+
+
+    def in_order(self):
+        def walk(root, data):
+            if not root:
+                return
+
+            walk(root.left, data)
+            data.append(root.value)
+            walk(root.right, data)
+
+        data_values = []
+        walk(self.root, data_values)
+        return data_values
+
+    def post_order(self):
+        def walk(root, data):
+            if not root:
+                return
+
+            walk(root.left, data)
+            walk(root.right, data)
+            data.append(root.value)
+
+        data_values = []
+        walk(self.root, data_values)
+        return data_values
 
 
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
