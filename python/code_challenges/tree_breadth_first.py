f88535594e14466285e07fb599f1da3a73f30582
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-10 13:58:35 -0700

    added functionality for empty trees

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,27 @@
+from types import NoneType
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
+def breadth_first(tree):
+    breadth = Queue()
+    tree_values = []
 
-def breadth_first():
-    pass
+    if not tree:
+        return tree_values
+    if not tree.root:
+        return tree_values
+    if not breadth.front:
+        breadth.enqueue(tree.root)
+    else:
+        while breadth.front:
+            root = breadth.dequeue()
+            tree_values.append(root.value)
+            if root.left:
+                breadth.enqueue(root.left)
+            else:
+                pass
+            if root.right:
+                breadth.enqueue(root.right)
+            else:
+                pass
+        return(tree_values)
