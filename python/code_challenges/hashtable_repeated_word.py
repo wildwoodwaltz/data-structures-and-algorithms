080aeac23eabacb85089169aa40875602b7964be
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-26 16:01:36 -0700

    tests pass

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,18 +1,15 @@
-from operator import contains
 import re
 
 def first_repeated_word(str):
  
     set_ = set()
-    str = re.sub(r'[^\w\s]','', str) 
-    print(str)
-
-    str = str.strip()
-    str = str.strip('\n')
+    str = re.sub(r'[^\w\s]','', str)
+    str = re.sub(r'\n', '', str) 
     str = str.split(" ")
- 
+    
+    print(str)
     for word in str:
-        if word.lower() in set_:
+        if word and word.lower() in set_:
             return word.lower()
         else:
             set_.add(word.lower())
