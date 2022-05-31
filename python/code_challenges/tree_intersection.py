475e81d65e1545e17de60e2beb21961d75442393
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-31 09:21:11 -0700

    added tree intersection

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,12 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree_a, tree_b):
+    list_ = BinaryTree.pre_order(tree_a)
+    shared_numbers = []
+
+    for num in list_:
+        if tree_b.contains(num):
+            shared_numbers.append(num)
+
+    return shared_numbers
